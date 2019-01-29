fb48fa03b8e76547aee387a64b9a2987402f0ad3
Zach Leach <zachleach@techoverco.com>
2019-01-29 09:42:17 -0700

    Added sunk method and it works.

--- lib/ship.rb
+++ lib/ship.rb
@@ -3,10 +3,12 @@ class Ship
   attr_reader :name,
               :length,
               :health
+
   def initialize(name, length)
     @name = name
     @length = length
     @health = length
+    @sunk = false
   end
 
   def health
@@ -19,10 +21,14 @@ class Ship
     elsif @health < length && sunk? != true
       @health -= 1
     else sunk? == true
+    end
   end
 
   def sunk?
-    false
+    if @health >= 1
+      false
+    elsif @health == 0
+      true
+    end
   end
-
 end
