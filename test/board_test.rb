548326c4e7b0546fdc58d3cec53791fdfb569422
Zach Leach <zachleach@techoverco.com>
2019-01-30 13:50:21 -0700

    Added test board can test placement based on length.

--- test/board_test.rb
+++ test/board_test.rb
@@ -2,12 +2,15 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/board'
 require './lib/cell'
+require './lib/ship'
 require 'pry'
 
 class BoardTest < Minitest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_board_exists
@@ -19,7 +22,7 @@ class BoardTest < Minitest::Test
     assert_equal ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"], @board.cells.keys
   end
 
-  def test_board_has_valid_coordinate
+  def test_board_has_valid_coordinates
     assert_equal true, @board.valid_coordinate?("A2")
     assert_equal true, @board.valid_coordinate?("A1")
     assert_equal false, @board.valid_coordinate?("A5")
@@ -27,4 +30,15 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?("A22")
     assert_equal true, @board.valid_coordinate?("B3")
   end
+
+  def test_board_can_test_placement_based_on_length
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+  end
 end
+
+# pry(main)> board.valid_placement?(cruiser, ["A1", "A2"])
+# # => false
+#
+# pry(main)> board.valid_placement?(submarine, ["A2", "A3", "A4"])
+# # => false
