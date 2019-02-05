c9c9d2bb6b9cb23841d1ecc49a8b41c81ee88633
Zach Leach <zachleach@techoverco.com>
2019-02-04 22:14:55 -0700

    Refactored game cli layout.

--- lib/game.rb
+++ lib/game.rb
@@ -15,12 +15,14 @@ class Game
   end
 
   def prompt_user_input
+    system('clear')
      puts "Welcome to BATTLESHIP
      Enter p to play. Enter q to quit."
      process_input
   end
 
   def process_input
+    system('clear')
     user_input = gets.chomp
     case user_input
     when "p"
@@ -31,22 +33,26 @@ class Game
       puts "#{"\n\n\n"}Invalid Input.#{"\n\n\n"}"
       prompt_user_input
     end
+  end
+
+  def cpu_ship_placement
+    cpu_placement_cruiser
 
   end
 
   def player_ship_placement
-        puts "I have laid out my ships on the grid.
-    You now need to lay out your two ships.
-    The Submarine is two units long and the
-    Cruiser is three units long."
+    system('clear')
+    print "I have laid out my ships on the grid.\n"
+    print "You now need to lay out your two ships.\n"
+    print "The Submarine is two units long and the\n"
+    print "Cruiser is three units long.\n"
     @player_board.render
-    cpu_placement_cruiser
     player_input_coordinates_cruiser
   end
 
   def player_input_coordinates_cruiser
-    puts "Enter the squares for the Cruiser (3 spaces):
-> "
+    puts "Enter the squares for the Cruiser (3 spaces):"
+
     cruiser_input = gets.chomp.upcase.split(" ")
     user_cruiser = Ship.new("Cruiser", 3)
     if @player_board.validation.valid_placement?(user_cruiser, cruiser_input)
@@ -61,8 +67,7 @@ class Game
   end
 
   def player_input_coordinates_submarine
-    puts "Enter the squares for the Submarine (2 spaces):
-    > "
+    puts "Enter the squares for the Submarine (2 spaces):"
     submarine_input = gets.chomp.upcase.split(" ")
     user_submarine = Ship.new("Submarine", 2)
 
@@ -70,12 +75,14 @@ class Game
       @player_board.place(user_submarine, submarine_input)
       @player_board.render(true)
     else
+      system('clear')
       p "Those are invalid coordinates. Please try again:"
       player_input_coordinates_submarine
     end
   end
 
   def player_turn
+    system('clear')
     p "=============COMPUTER BOARD============="
     @player_board.render
     p "==============PLAYER BOARD=============="
@@ -102,10 +109,34 @@ class Game
     else
       return new_values
     end
+    cpu_cruiser = Ship.new("Cruiser", new_values)
   end
 
 
   def computer_turn
+
+  end
+
+  def cpu_cruiser_generator
+    random_values = []
+    random_values << @cpu_board.render_master.values.flatten.sample
+    random_values.each do |value|
+      if random_values.count < @cpu_cruiser.length
+        random_values << @cpu_board.render_master.values.flatten.sample
+      end
+    end
+    return random_values
+  end
+
+  def cpu_placement_cruiser
+    cpu_cruiser_generator
+    new_values = cpu_cruiser_generator
+    if @cpu_board.validation.valid_placement?(@cpu_cruiser, new_values) == false
+      cpu_placement_cruiser
+    else
+      return new_values
+    end
+    cpu_cruiser = Ship.new("Cruiser", new_values)
   end
 
 end
